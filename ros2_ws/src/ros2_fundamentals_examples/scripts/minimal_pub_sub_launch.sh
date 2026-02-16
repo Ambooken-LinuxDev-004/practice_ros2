69171ce383d17fbfb358dd8fe4abecbc6f83ed0a
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2026-02-16 06:49:09 +0530

    Learned Subscriber node in python

---
+++ ros2_ws/src/ros2_fundamentals_examples/scripts/minimal_pub_sub_launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Launch publisher & subscriber nodes with cleanup handling
+
+cleanup(){
+  echo "Restarting ROS 2 daemon to cleanup before shutting down all processes..."
+  ros2 daemon stop
+  sleep 1
+  ros2 daemon start
+  echo "Terminating all ROS-2 related processes.."
+  kill 0
+  exit
+}
+
+
+trap 'cleanup' SIGINT
+
+# Launch publisher node
+
+ros2 run ros2_fundamentals_examples py_minimal_publisher.py &
+
+sleep 2
+
+# Launch subscriber node
+
+ros2 run ros2_fundamentals_examples py_minimal_subscriber.py
